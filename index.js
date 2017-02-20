d36242004a5ab12fcf2eb925756c4d15b02cf414
bsudekum <bobby@mapbox.com>
2017-02-20 14:00:10 -0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var builder = new xml2js.Builder();
 var tinycolor = require('tinycolor2');
 var constants = require('./lib/constants');
 var assets = require('./lib/loadAssets');
+var allColors = [];
+for (var i in tinycolor.names) {
+    allColors.push(i);
+}
 
 function generateMarker (options, callback) {
     if (options.size && (options.size !== 's' && options.size !== 'l')) return callback('Invlaid size');
@@ -35,12 +39,16 @@ function generateMarker (options, callback) {
     if (!tinyTint.isValid()) return callback('Invalid color');
     var tintIsLightInColor = tinyTint.isLight();
 
+    var markerPaths = backgroundMarkerSize.svg.g[0].g[0].path;
     // Change the tint on the marker background
-    backgroundMarkerSize.svg.g[0].g[0].path[0].$.fill = tint;
+    markerPaths[markerPaths.length - 2].$.fill = tinyTint.toHexString();
 
-    // If the background color is light, apply a light tint to the icon or text to make it stand out more
+    // Swap the border color if the tint is light and there is a symbol
+    markerPaths[markerPaths.length - 1].$.fill = tinycolor.mostReadable(tint, allColors).toHexString();
+
     // Some Maki icons have different SVG makeups. This attempts to apply the tint to the correct path
     if (backgroundMarkerSize.svg.g[0].g[1].g[0].path) {
+        // If the background color is light, apply a light tint to the icon or text to make it stand out more
         backgroundMarkerSize.svg.g[0].g[1].g[0].path[0].$.style = tintIsLightInColor ? `fill:${constants.DEFAULT_BLACK}` : `fill:${constants.DEFAULT_WHITE}`;
     } else {
         backgroundMarkerSize.svg.g[0].g[1].g[0].$.fill = tintIsLightInColor ? constants.DEFAULT_BLACK : constants.DEFAULT_WHITE;
